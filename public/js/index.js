7aabbd9123172eaeee6bfd450dc27b08932c08cd
kelvintran05 <tctri@tma.com.vn>
2018-03-07 15:45:27 +0700

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,15 +3,15 @@ var socket = io();
 socket.on('connect',()=>{
     console.log('Connected to server');
 
-    socket.emit('createEmail',{
-        to:'dat@gmail.com',
-        text: 'Hey.This is kelvin'
-    });
+    // socket.emit('createEmail',{
+    //     to:'dat@gmail.com',
+    //     text: 'Hey.This is kelvin'
+    // });
 
-    socket.emit('createMessage',{
-        from:'Andrew',
-        text:'Yup, that works for me.'
-    });
+    // socket.emit('createMessage',{
+    //     from:'Andrew',
+    //     text:'Yup, that works for me.'
+    // });
 });
 
 socket.on('disconnect',()=> {
@@ -22,6 +22,6 @@ socket.on('newMessage',(message)=>{
     console.log('newMessage',message);
 });
 
-socket.on('newEmail', (email)=>{
-    console.log('New email',email);
-});
+// socket.on('newEmail', (email)=>{
+//     console.log('New email',email);
+// });
--- server/server.js
+++ server/server.js
@@ -15,28 +15,33 @@ app.use(express.static(publicPath));
 io.on('connection',(socket) => {
     console.log('New user connected!');
 
-    socket.emit('newEmail',{
-        from: 'kelvintran95@gmail.com',
-        text: 'Hello!!!',
-        createAt: 123
-    });
+    // socket.emit('newEmail',{
+    //     from: 'kelvintran95@gmail.com',
+    //     text: 'Hello!!!',
+    //     createAt: 123
+    // });
 
-    socket.emit('newMessage',{
-        from: 'kelvin',
-        text:'See you then',
-        createAt:1234
-    });
+    // socket.emit('newMessage',{
+    //     from: 'kelvin',
+    //     text:'See you then',
+    //     createAt:1234
+    // });
 
     socket.on('createMessage',(message)=>{
         console.log('createMessage',message);
+        io.emit('newMessage',{
+            from: message.from,
+            text: message.text,
+            careateAt:new Date().getTime()
+        });
     });
-    socket.on('createEmail',(newEmail)=>{
-        console.log('createEmail',newEmail);
-    });
+    // socket.on('createEmail',(newEmail)=>{
+    //     console.log('createEmail',newEmail);
+    // });
 
-    socket.on('disconnect', ()=>{
-        console.log('User was disconnected!');
-    });
+    // socket.on('disconnect', ()=>{
+    //     console.log('User was disconnected!');
+    // });
 });
 server.listen(port, ()=> {
     console.log(`Server in up on ${port}`);
